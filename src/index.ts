3f62159f218ef7353effda7109c2d1b89238079d
SRINJOY DAS <dassrinjoy333@gmail.com>
2024-09-09 17:34:03 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { PrismaClient } from "@prisma/client/extension";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export interface Env {
+	DATABASE_URL : string
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async fetch(
+		request : Request,
+		env : Env,
+		ctx : ExecutionContext
+	) : Promise<Response> {
+		const prisma = new PrismaClient({
+			datasourceUrl : env.DATABASE_URL,
+		}).$extends(withAccelerate())
+
+		await prisma.log.create({ //putting things in the database //when someone is using outr site then a new log will be created
+			data : {
+				level : 'Info',
+				message : `${request.method} ${request.url}`,
+				meta : {
+					headers : JSON.stringify(request.headers),
+				},
+			},
+		})
+
+		const {data , info} = await prisma.log //check the logs that any user is there with the same logs or not
+			.findMany({
+				take : 20,
+				orderBy : {
+					id : 'desc',
+				},
+			})
+			.withAccelerateInfo()
+
+			console.log(JSON.stringify(info)); //also logging the info 
+
+			return new Response(`request method : ${request.method}!`);
+	}
+}
